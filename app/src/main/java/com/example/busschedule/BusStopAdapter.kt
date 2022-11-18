4ecabe3241bbf9f4657a31873e519b47a4ee4e2e
achevauchet <arthur.chevauchet@apside-groupe.com>
2022-11-18 11:49:45 +0100

    flow + cleanup

--- app/src/main/java/com/example/busschedule/BusStopAdapter.kt
+++ app/src/main/java/com/example/busschedule/BusStopAdapter.kt
@@ -9,16 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.databinding.BusStopItemBinding
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.*
 
-class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) :
+    ListAdapter<Schedule, BusStopAdapter.BusStopViewHolder>(DiffCallback) {
 
-    class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+    class BusStopViewHolder(private var binding: BusStopItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SimpleDateFormat")
         fun bind(schedule: Schedule) {
             binding.stopNameTextView.text = schedule.stopName
             binding.arrivalTimeTextView.text = SimpleDateFormat(
-                "h:mm a").format(Date(schedule.arrivalTime.toLong() * 1000))
+                "h:mm a"
+            ).format(Date(schedule.arrivalTime.toLong() * 1000))
         }
     }
 
--- app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -21,16 +21,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.StopScheduleFragmentBinding
 import com.example.busschedule.viewmodels.BusScheduleViewModel
 import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class StopScheduleFragment: Fragment() {
+class StopScheduleFragment : Fragment() {
 
     companion object {
         var STOP_NAME = "stopName"
@@ -75,8 +74,10 @@ class StopScheduleFragment: Fragment() {
         val busStopAdapter = BusStopAdapter {}
         recyclerView.adapter = busStopAdapter
 
-        GlobalScope.launch(Dispatchers.IO) {
-            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+        lifecycle.coroutineScope.launch {
+            viewModel.scheduleForStopName(stopName).collect {
+                busStopAdapter.submitList(it)
+            }
         }
     }
 
